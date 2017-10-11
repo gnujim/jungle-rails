8d803b2ae02f9a44aa23ea09cc73eb06a6a4c18f
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-11 16:59:36 -0700

    implemented login and sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,19 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show] 
-  resources :users, only: [:new, :create]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:show, :edit, :update, :destroy]
   end
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
